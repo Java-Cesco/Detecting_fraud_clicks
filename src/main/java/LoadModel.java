fef8eb4974884c8bdc3e6fe41a1da7ec497736b6
hyungyun.Moon <mhg5303@gmail.com>
2018-06-12 20:16:13 +0900

    implement basic load

---
+++ src/main/java/LoadModel.java
@@ -0,0 +1,17 @@
+import org.apache.spark.ml.PipelineModel;
+import org.apache.spark.ml.regression.DecisionTreeRegressionModel;
+import org.apache.spark.sql.SparkSession;
+
+public class LoadModel {
+    public static void main(String args[]) {
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Detecting Fraud Clicks")
+                .master("local")
+                .getOrCreate();
+
+        PipelineModel model = PipelineModel.load("decisionTree.model");
+//        DecisionTreeRegressionModel treeModel = (DecisionTreeRegressionModel) (model.stages()[1]);
+//        System.out.println(treeModel.toDebugString());
+    }
+}
